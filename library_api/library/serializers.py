df5866a2803fb3d5ff9e4754ef3710a7aa1edc89
Barrack <barrackdome45@gmail.com>
2025-12-14 13:11:14 +0300

    Implement User and Book serializers

---
+++ library_api/library/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User, Book
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = '__all__'
+        
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
